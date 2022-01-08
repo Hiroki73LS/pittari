d420bd1a33f8b08158b77f998ead18e1712a7c9f
Hiroki mac air <aichan_lovepower@yahoo.co.jp>
2022-01-08 15:46:59 +0900

    Initial Commit

---
+++ Shared/PittariApp.swift
@@ -0,0 +1,17 @@
+//
+//  PittariApp.swift
+//  Shared
+//
+//  Created by 林宏樹 on 2022/01/08.
+//
+
+import SwiftUI
+
+@main
+struct PittariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
